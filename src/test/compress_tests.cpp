d752d95389b4daa0c57e4d3a06bb5c96eb667067
hanacoinproject <hanacoinproject@gmail.com>
2018-01-17 14:27:10 +1100

    Change coin amount

--- src/test/compress_tests.cpp
+++ src/test/compress_tests.cpp
@@ -44,7 +44,7 @@ BOOST_AUTO_TEST_CASE(compress_amounts)
     BOOST_CHECK(TestPair(         CENT,       0x7));
     BOOST_CHECK(TestPair(         COIN,       0x9));
     BOOST_CHECK(TestPair(      50*COIN,      0x32));
-    BOOST_CHECK(TestPair(105120000*COIN, 0x6440100));
+    BOOST_CHECK(TestPair(122500000*COIN, 0x74D33A0));
 
     for (uint64_t i = 1; i <= NUM_MULTIPLES_UNIT; i++)
         BOOST_CHECK(TestEncode(i));
