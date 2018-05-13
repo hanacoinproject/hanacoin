b7da2b8d63427dc1c47dd89aa2b3e11b1949b003
hanacoinproject <hanacoinproject@gmail.com>
2018-05-13 12:54:52 +1000

    Update chainparams and premine configurations

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -46,6 +46,8 @@ struct Params {
     int BIP65Height;
     /** Block height at which BIP66 becomes active */
     int BIP66Height;
+    /** HanaCoin premine blocks */
+    int premineBlocks;
     /**
      * Minimum blocks including miner confirmation of the total of 2016 blocks in a retargeting period,
      * (nPowTargetTimespan / nPowTargetSpacing) which is also used for BIP9 deployments.
