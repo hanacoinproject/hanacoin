553d913a5cf28bc5b80ec57108d59ae47258fe76
hanacoinproject <hanacoinproject@gmail.com>
2018-11-05 15:07:16 +1100

    Adding .net as dnsseed

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -140,6 +140,7 @@ public:
 
         // Note that of those with the service bits flag, most only support a subset of possible options
         vSeeds.emplace_back("dnsseed.hanacoin.org");
+        vSeeds.emplace_back("dnsseed.hanacoin.net");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 40);  // H
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
